103736894834e89c392fb91597f45bd9fc7b9b15
username <wecp-dummy@gmail.com>
2025-08-30 11:11:53 +0000

    Day 2 Task

--- backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplArraylist.java
@@ -1,5 +1,40 @@
 package com.wecp.progressive.service.impl;
-
-public class CricketerServiceImplArraylist  {
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import com.wecp.progressive.entity.Cricketer;
+import com.wecp.progressive.service.CricketerService;
+public class CricketerServiceImplArraylist  implements CricketerService {
+   public static List<Cricketer>cricketerList=new ArrayList<>();
+    @Override
+    public Integer addCricketer(Cricketer cricketer) {
+      cricketerList.add(cricketer);
+      return cricketerList.size();
+    }
+    @Override
+    public Cricketer getCricketerById(int cricketerId) {
+     return null;
+    }
+    @Override
+    public void updateCricketer(Cricketer cricketer) {
+    }
+    @Override
+    public void deleteCricketer(int cricketerId) {  
+    }
+    public List<Cricketer> getAllCricketersSortedByExperience()
+{
+  List<Cricketer> sortedCricketer= cricketerList;
+  sortedCricketer.sort(Comparator.comparing(Cricketer::getExperience));
+  return sortedCricketer;
+}
+    @Override
+    public List<Cricketer> getAllCricketers() {
+       return cricketerList;
+    }
+@Override
+public void emptyArrayList()
+{
+  cricketerList=new ArrayList<>();
+}
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
@@ -1,5 +1,43 @@
 package com.wecp.progressive.service.impl;
 
-public class MatchServiceImplJdbc  {
+import java.util.List;
+
+import com.wecp.progressive.dao.MatchDAO;
+import com.wecp.progressive.entity.Match;
+import com.wecp.progressive.service.MatchService;
+
+public class MatchServiceImplJdbc  implements MatchService{
+
+  private MatchDAO matchDAO;
+  
+    public MatchServiceImplJdbc(MatchDAO matchDAO) {
+    this.matchDAO = matchDAO;
+  }
+
+    @Override
+    public Integer addMatch(Match match) {
+      return -1;
+    }
+
+    @Override
+    public Match getMatchById(int matchId) {
+       return null;
+    }
+
+    @Override
+    public void updateMatch(Match match) {
+        
+    }
+
+    @Override
+    public void deleteMatch(int matchId) {
+       
+    }
+
+    @Override
+    public List<Match> getAllMatches() {
+      return List.of();
+    }
+
 
 }
