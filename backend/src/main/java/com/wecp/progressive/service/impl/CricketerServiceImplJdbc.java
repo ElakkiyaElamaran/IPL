4b5c06f3a1b9bab9d4d0a449854a667718285b46
username <wecp-dummy@gmail.com>
2025-08-31 05:11:22 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplJdbc.java
@@ -1,5 +1,45 @@
 package com.wecp.progressive.service.impl;
 
-public class CricketerServiceImplJdbc  {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import com.wecp.progressive.dao.CricketerDAO;
+import com.wecp.progressive.entity.Cricketer;
+import com.wecp.progressive.service.CricketerService;
+
+public class CricketerServiceImplJdbc implements CricketerService {
+    private CricketerDAO cricketerDAO;
+
+    public CricketerServiceImplJdbc(CricketerDAO cricketerDAO) {
+        this.cricketerDAO = cricketerDAO;
+    }
+
+    @Override
+    public List<Cricketer> getAllCricketers() {
+      return List.of();
+    }
+
+    @Override
+    public Integer addCricketer(Cricketer cricketer) {
+       return -1;
+    }
+
+    @Override
+    public List<Cricketer> getAllCricketersSortedByExperience() {
+        return List.of();
+    }
+    @Override
+    public void updateCricketer(Cricketer cricketer) {
+    }
+    @Override
+    public void deleteCricketer(int cricketerId) {  
+    }
+    @Override
+    public Cricketer getCricketerById(int cricketerId) {
+     return null;
+    
+
+}
 }
