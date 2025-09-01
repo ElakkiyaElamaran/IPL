b478546358beb92eb477e18adbbe7118e7cd52f2
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-01 17:11:58 +0530

    Update TeamDAOImpl.java

--- backend/src/main/java/com/wecp/progressive/dao/TeamDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/TeamDAOImpl.java
@@ -1,6 +1,5 @@
 package com.wecp.progressive.dao;
 
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,192 +7,172 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.classmate.ResolvedType;
 import com.wecp.progressive.config.DatabaseConnectionManager;
 import com.wecp.progressive.entity.Team;
 
-public class TeamDAOImpl implements TeamDAO {
+public class TeamDAOImpl implements TeamDAO{
 
     @Override
-    public int addTeam(Team team) throws SQLException{
+    public int addTeam(Team team) throws SQLException {
+        Connection connection=null;
+        PreparedStatement statement=null;
+        int generatedID=-1;
+        try{
+            connection=DatabaseConnectionManager.getConnection();
+            String sql="INSERT INTO team(team_name, location, owner_name, establishment_year) VALUES(?,?,?,?)";
+            statement=connection.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS);
+
+            statement.setString(1, team.getTeamName());
+            statement.setString(2, team.getLocation());
+            statement.setString(3, team.getOwnerName());
+            statement.setInt(4, team.getEstablishmentYear());
+
+            statement.executeUpdate();
+
+            ResultSet resultSet=statement.getGeneratedKeys();
+            if(resultSet.next()){
+                generatedID=resultSet.getInt(1);
+                team.setTeamId(generatedID);
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw e;
+        }
+        finally{
+            if(statement!=null){
+                statement.close();
+            }
+            if(connection!=null){
+                connection.close();
+            }
+        }
+        return generatedID;
+    }
+
+    @Override
+    public Team getTeamById(int teamId) throws SQLException{
+        Connection connection=null;
+        PreparedStatement statement=null;
+        ResultSet resultSet=null;
+        try{
+            connection=DatabaseConnectionManager.getConnection();
+            String sql="SELECT * FROM team WHERE team_id=?";
+            statement=connection.prepareStatement(sql);
+            statement.setInt(1,teamId);
+            resultSet=statement.executeQuery();
+
+            if(resultSet.next()){
+                String teamName= resultSet.getString("team_name");
+                String location=resultSet.getString("location");
+                String ownerName= resultSet.getString("owner_name");
+                int establishmentYear=resultSet.getInt("establishment_year");
+
+                return new Team(teamId, teamName, location, ownerName, establishmentYear);
+            }
+
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw e;
+        }
+        finally{
+            if(resultSet!=null){
+                resultSet.close();
+            }
+            if(statement!=null){
+                statement.close();
+            }
+            if(connection!=null){
+                connection.close();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void updateTeam(Team team) throws SQLException{
+        Connection connection=null;
+        PreparedStatement statement=null;
+        try{
+            connection=DatabaseConnectionManager.getConnection();
+            String sql="UPDATE team SET team_name=?, location=?, owner_name=?, establishment_year=? WHERE team_id=?";
+            statement=connection.prepareStatement(sql);
+            statement.setString(1, team.getTeamName());
+            statement.setString(2, team.getLocation());
+            statement.setString(3, team.getOwnerName());
+            statement.setInt(4, team.getEstablishmentYear());
+            statement.setInt(5, team.getTeamId());
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw e;
+        }
+        finally{
+            if(connection!=null){
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public void deleteTeam(int teamId) throws SQLException{
+        Connection connection=null;
+        PreparedStatement statement=null;
+        try{
+            connection=DatabaseConnectionManager.getConnection();
+            String sql="DELETE FROM team WHERE team_id=?";
+            statement=connection.prepareStatement(sql);
+            statement.setInt(1,teamId);
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw e;
+        }
+        finally{
+            if(connection!=null){
+                connection.close();
+            }
+        }
+        
+    }
+
+    @Override
+    public List<Team> getAllTeams() throws SQLException{
+       List<Team> teams=new ArrayList<>();
        Connection connection=null;
-      PreparedStatement statement=null;
-      ResultSet rs=null;
-      int generatedID=-1;
-       try
-       {
+       PreparedStatement statement=null;
+       ResultSet resultSet=null;
+
+       try{
         connection=DatabaseConnectionManager.getConnection();
-        String sql="insert into team(team_name,location,owner_name,establishment_year) values(?,?,?,?)";
-        statement=connection.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS);
-        statement.setString(1, team.getTeamName());
-        statement.setString(2,team.getLocation());
-        statement.setString(3, team.getOwnerName());
-        statement.setInt(4, team.getEstablishmentYear());
-        statement.executeUpdate();
-       rs=statement.getGeneratedKeys();
-        if(rs.next())
-        {
-         generatedID=rs.getInt(1);
-         team.setTeamId(generatedID);
+        String sql="SELECT * FROM team";
+        statement=connection.prepareStatement(sql);
+        resultSet=statement.executeQuery();
+
+        while(resultSet.next()){
+            int teamId=resultSet.getInt("team_id");
+            String teamName=resultSet.getString("team_name");
+            String location=resultSet.getString("owner_name");
+            String ownerName= resultSet.getString("owner_name");
+            int establishmentYear=resultSet.getInt("establishment_year");
+            teams.add(new Team(teamId, teamName, location, ownerName, establishmentYear));
         }
        }
-       catch (SQLException e) {
-         e.printStackTrace();
-         throw e;
-      }
-      finally
-      {
-       if(statement!=null)
-       {
-         statement.close();
+       catch(SQLException e){
+        e.printStackTrace();
+        throw e;
        }
-       if(connection!=null)
-       {
-         connection.close();
-       }
-      }
-      return generatedID;
-    }
-
-    @Override
-    public Team getTeamById(int teamId)  throws SQLException {
-      Connection connection=null;
-      PreparedStatement statement=null;
-       try
-       {
-        connection=DatabaseConnectionManager.getConnection();
-        String sql="select * from team where team_id=?";
-        statement=connection.prepareStatement(sql);
-        statement.setInt(1, teamId);
-        ResultSet rs=statement.executeQuery();
-        if(rs.next())
-        {
-         String teamName=rs.getString("team_name");
-         String location=rs.getString("location");
-         String ownerName=rs.getString("owner_name");
-         int establishmentYear=rs.getInt("establishment_year");
-         return new Team(teamId, teamName, location, ownerName, establishmentYear);
+       finally{
+        if(connection!=null){
+            connection.close();
         }
        }
-       catch (SQLException e) {
-         e.printStackTrace();
-         throw e;
-      }
-      finally
-      {
-       if(statement!=null)
-       {
-         statement.close();
-       }
-       if(connection!=null)
-       {
-         connection.close();
-       }
-      }
-      return null;
+       return teams;
     }
-
-    @Override
-    public void updateTeam(Team team)  throws SQLException{
-      Connection connection=null;
-      PreparedStatement statement=null;
-       try
-       {
-        connection=DatabaseConnectionManager.getConnection();
-        String sql="update team set team_name=?,location=?,owner_name=?,establishment_year=? where team_id= ? ";
-        statement=connection.prepareStatement(sql);
-        statement.setString(1,team.getTeamName());
-        statement.setString(2, team.getLocation());
-        statement.setString(3, team.getOwnerName());
-        statement.setInt(4, team.getEstablishmentYear());
-        statement.setInt(5, team.getTeamId());
-        statement.executeUpdate();
-       }
-       catch (SQLException e) {
-         e.printStackTrace();
-         throw e;
-      }
-      finally
-      {
-       if(statement!=null)
-       {
-         statement.close();
-       }
-       if(connection!=null)
-       {
-         connection.close();
-       }
-      }
-    }
-
-    @Override
-    public void deleteTeam(int teamId)  throws SQLException{
-      Connection connection=null;
-      PreparedStatement statement=null;
-       try
-       {
-        connection=DatabaseConnectionManager.getConnection();
-        String sql="delete from team where team_id=?";
-        statement=connection.prepareStatement(sql);
-        statement.setInt(1,teamId);
-        statement.executeUpdate();
-       }
-       catch (SQLException e) {
-         e.printStackTrace();
-         throw e;
-      }
-      finally
-      {
-       if(statement!=null)
-       {
-         statement.close();
-       }
-       if(connection!=null)
-       {
-         connection.close();
-       }
-      }
-      
-    }
-    @Override
-    public List<Team> getAllTeams()  throws SQLException{
-      List<Team> teams=new ArrayList<>();
-      Connection connection=null;
-      PreparedStatement statement=null;
-      ResultSet rs=null;
-      try
-      {
-       connection=DatabaseConnectionManager.getConnection();
-       String sql="select * from team";
-       statement=connection.prepareStatement(sql);
-       rs=statement.executeQuery();
-       while(rs.next())
-       {
-         int teamId=rs.getInt("team_id");
-         String teamName=rs.getString("team_name");
-         String location=rs.getString("location");
-         String ownerName=rs.getString("owner_name");
-         int establishmentYear=rs.getInt("establishment_year");
-         teams.add(new Team(teamId, teamName, location, ownerName, establishmentYear));
-       }
-      }
-      catch (SQLException e) {
-         e.printStackTrace();
-         throw e;
-      }
-      finally
-      {
-       if(statement!=null)
-       {
-         statement.close();
-       }
-       if(connection!=null)
-       {
-         connection.close();
-       }
-      }
-      return teams;
-    }
+    
 
 
 
