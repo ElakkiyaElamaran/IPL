f096ce0b0ecc1882cafa18498a32bbc7807d023a
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-01 17:16:38 +0530

    Update TeamService.java

--- backend/src/main/java/com/wecp/progressive/service/TeamService.java
+++ backend/src/main/java/com/wecp/progressive/service/TeamService.java
@@ -7,22 +7,22 @@ import java.util.List;
 
 public interface TeamService {
 
-    List<Team> getAllTeams()throws SQLException;
+    List<Team> getAllTeams() throws SQLException;
 
-    int addTeam(Team team)throws SQLException;
+    int addTeam(Team team) throws SQLException;
 
-    List<Team> getAllTeamsSortedByName()throws SQLException;
+    List<Team> getAllTeamsSortedByName() throws SQLException;
 
-    default void emptyArrayList() throws SQLException{
+    default void emptyArrayList()throws SQLException {
     }
 
     //Do not implement these methods in TeamServiceImplArrayList.java class
-    default Team getTeamById(int teamId) throws SQLException{
+    default Team getTeamById(int teamId)throws SQLException {
         return null;
     }
 
-    default void updateTeam(Team team) throws SQLException{}
+    default void updateTeam(Team team)throws SQLException {}
 
-    default void deleteTeam(int teamId)throws SQLException {}
+    default void deleteTeam(int teamId) throws SQLException{}
 
 }
