7d95c414b71ec87627cc569d349fc2253e8fd2fa
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-02 17:19:47 +0530

    Update TeamServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
@@ -1,52 +1,89 @@
 package com.wecp.progressive.service.impl;
-
-import com.wecp.progressive.dao.TeamDAO;
-import com.wecp.progressive.entity.Team;
-import com.wecp.progressive.service.TeamService;
-
+ 
 import java.sql.SQLException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
-public class TeamServiceImplJdbc implements TeamService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-    private TeamDAO teamDAO;
+import com.wecp.progressive.dao.TeamDAO;
+import com.wecp.progressive.entity.Team;
+import com.wecp.progressive.exception.TeamAlreadyExistsException;
+import com.wecp.progressive.exception.TeamDoesNotExistException;
+import com.wecp.progressive.repository.CricketerRepository;
+import com.wecp.progressive.repository.MatchRepository;
+import com.wecp.progressive.repository.TeamRepository;
+import com.wecp.progressive.repository.TicketBookingRepository;
+import com.wecp.progressive.repository.VoteRepository;
+import com.wecp.progressive.service.TeamService;
+ 
+@Service
+public class TeamServiceImplJpa  implements TeamService {
 
-    public TeamServiceImplJdbc(TeamDAO teamDAO) {
-        this.teamDAO = teamDAO;
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private CricketerRepository cricketerRepository;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+      @Autowired
+    private TicketBookingRepository ticketBookingRepository;
+
+    @Autowired
+    public TeamServiceImplJpa(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
     }
 
     @Override
     public List<Team> getAllTeams() throws SQLException {
-        return teamDAO.getAllTeams();
+        return teamRepository.findAll();
     }
 
     @Override
     public int addTeam(Team team) throws SQLException {
-        return teamDAO.addTeam(team);
+
+        if(teamRepository.findByTeamName(team.getTeamName()) != null){
+            throw new TeamAlreadyExistsException("Team with same name exists");
+        }
+        return teamRepository.save(team).getTeamId();
     }
 
     @Override
     public List<Team> getAllTeamsSortedByName() throws SQLException {
-        List<Team> sortedTeams = teamDAO.getAllTeams();
-        if (!sortedTeams.isEmpty()) {
-            sortedTeams.sort(Comparator.comparing(Team::getTeamName));
-        }
-        return sortedTeams;
+        List<Team> sortedTeam = teamRepository.findAll();
+        sortedTeam.sort(Comparator.comparing(Team::getTeamName));
+        return sortedTeam;
     }
 
     @Override
     public Team getTeamById(int teamId) throws SQLException {
-        return teamDAO.getTeamById(teamId);
+        if(teamRepository.findByTeamId(teamId) == null){
+            throw new TeamDoesNotExistException("Team does not exist");
+        }
+        return teamRepository.findByTeamId(teamId);
     }
 
     @Override
     public void updateTeam(Team team) throws SQLException {
-        teamDAO.updateTeam(team);
+        if(teamRepository.findByTeamName(team.getTeamName()) != null){
+            throw new TeamAlreadyExistsException("Team with same name exists");
+        }
+        teamRepository.save(team);
     }
 
     @Override
     public void deleteTeam(int teamId) throws SQLException {
-        teamDAO.deleteTeam(teamId);
+        voteRepository.deleteByTeamId(teamId);
+        ticketBookingRepository.deleteByTeamId(teamId);
+        matchRepository.deleteByTeamId(teamId);
+        cricketerRepository.deleteByTeamId(teamId);
+        teamRepository.deleteById(teamId);
     }
 }
