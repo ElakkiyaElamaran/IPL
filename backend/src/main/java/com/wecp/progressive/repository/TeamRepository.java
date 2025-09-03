b5a595a2697307619e1a86913e5666f3c784d8e1
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-03 18:20:27 +0530

    Update TeamRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
@@ -11,5 +11,5 @@ import org.springframework.stereotype.Repository;
 public interface TeamRepository extends JpaRepository<Team, Integer> {
 
     Team findByTeamId(int teamId);
-    Team findByTeamName(String teamName);
+    
 }
