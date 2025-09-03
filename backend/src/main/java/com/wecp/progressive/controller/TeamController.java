2f0582dd02724775ba5966935ea252557c1e4ce8
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-03 18:28:23 +0530

    Update TeamController.java

--- backend/src/main/java/com/wecp/progressive/controller/TeamController.java
+++ backend/src/main/java/com/wecp/progressive/controller/TeamController.java
@@ -1,17 +1,25 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Team;
-import com.wecp.progressive.exception.TeamAlreadyExistsException;
-import com.wecp.progressive.exception.TeamDoesNotExistException;
+import com.wecp.progressive.service.TeamService;
 import com.wecp.progressive.service.impl.TeamServiceImplArraylist;
 import com.wecp.progressive.service.impl.TeamServiceImplJpa;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ObjectInputFilter.Status;
+import java.sql.SQLClientInfoException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,86 +27,63 @@ import java.util.List;
 public class TeamController {
 
     @Autowired
-    TeamServiceImplArraylist teamServiceImplArraylist;
+    private TeamServiceImplJpa teamServiceImplJpa;
 
     @Autowired
-    TeamServiceImplJpa teamServiceImplJpa;
+    private TeamServiceImplArraylist teamArrayListService;
+
 
     @GetMapping
-    public ResponseEntity<List<Team>> getAllTeams() {
-        try {
-            List<Team> teamList = teamServiceImplJpa.getAllTeams();
-            return new ResponseEntity<>(teamList, HttpStatus.OK);
-        } catch (SQLException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<List<Team>> getAllTeams() throws SQLException {
+        return ResponseEntity.ok(teamServiceImplJpa.getAllTeams());
     }
 
     @GetMapping("/{teamId}")
-    public ResponseEntity<Team> getTeamById(@PathVariable int teamId) {
-        try {
-            Team team = teamServiceImplJpa.getTeamById(teamId);
-            return new ResponseEntity<>(team, HttpStatus.OK);
-        }catch(TeamDoesNotExistException t){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } 
-        catch (SQLException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Team> getTeamById( @PathVariable int teamId) throws SQLException{
+        Team team = teamServiceImplJpa.getTeamById(teamId);
+        return team !=null ? ResponseEntity.ok(team): ResponseEntity.noContent().build();
     }
 
+   
     @PostMapping
     public ResponseEntity<Integer> addTeam(@RequestBody Team team) {
-        try {
-            int teamId = teamServiceImplJpa.addTeam(team);
-            return new ResponseEntity<>(teamId, HttpStatus.CREATED);
-        }catch(TeamAlreadyExistsException t){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } 
-        catch (SQLException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+       try {
+        int teamId = teamServiceImplJpa.addTeam(team);
+        return new ResponseEntity<>(teamId,HttpStatus.CREATED);
+       }
+       catch(SQLException e)
+       {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+       }
+   
+   
     }
 
     @PutMapping("/{teamId}")
-    public ResponseEntity<Void> updateTeam(@PathVariable int teamId, @RequestBody Team team) {
-        try {
-            team.setTeamId(teamId);
-            teamServiceImplJpa.updateTeam(team);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(TeamAlreadyExistsException t){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } 
-        catch (SQLException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Void> updateTeam( @PathVariable int teamId, @RequestBody Team team) throws SQLException {
+        team.setTeamId(teamId);
+        teamServiceImplJpa.updateTeam(team);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{teamId}")
-    public ResponseEntity<Void> deleteTeam(@PathVariable int teamId) {
-        try {
-            teamServiceImplJpa.deleteTeam(teamId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (SQLException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Void> deleteTeam( @PathVariable int teamId) throws SQLException{
+        teamServiceImplJpa.deleteTeam(teamId);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/fromArrayList")
-    public ResponseEntity<List<Team>> getAllTeamsFromArrayList() {
-        List<Team> teamList = teamServiceImplArraylist.getAllTeams();
-        return new ResponseEntity<>(teamList, HttpStatus.OK);
-    }
-
-    @PostMapping("/toArrayList")
-    public ResponseEntity<Integer> addTeamToArrayList(@RequestBody Team team) {
-        int listSize = teamServiceImplArraylist.addTeam(team);
-        return new ResponseEntity<>(listSize, HttpStatus.CREATED);
+    public ResponseEntity<List<Team>> getAllTeamsFromArrayList() throws SQLException {
+        return ResponseEntity.ok(teamArrayListService.getAllTeams());
     }
 
     @GetMapping("/fromArrayList/sorted")
-    public ResponseEntity<List<Team>> getAllTeamsSortedByNameFromArrayList() {
-        List<Team> teamList = teamServiceImplArraylist.getAllTeamsSortedByName();
-        return new ResponseEntity<>(teamList, HttpStatus.OK);
+    public ResponseEntity<List<Team>> getAllTeamsSortedByNameFromArrayList() throws SQLException{
+        return ResponseEntity.ok(teamArrayListService.getAllTeamsSortedByName());
+    }
+
+    @PostMapping("/toArrayList")
+    public ResponseEntity<Integer> addTeamToArrayList(@RequestBody Team team) throws SQLException {
+        return ResponseEntity.status(201).body(teamArrayListService.addTeam(team));
     }
 }
