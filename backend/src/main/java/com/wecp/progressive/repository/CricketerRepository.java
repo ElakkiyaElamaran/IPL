ba5df02ed165ff9514c132874e3e8a4f1e1ae5f1
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-03 18:19:20 +0530

    Update CricketerRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/CricketerRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/CricketerRepository.java
@@ -14,14 +14,5 @@ import com.wecp.progressive.entity.Cricketer;
 @Repository
 public interface CricketerRepository extends JpaRepository<Cricketer, Integer> {
 
-    Cricketer findByCricketerId(int cricketerId);
-
-    List<Cricketer> findByTeam_TeamId(int teamId);
-
-    long countByTeam_TeamId(int teamId);
-
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM Cricketer c WHERE c.team.teamId = :teamId")
-    void deleteByTeamId(@Param("teamId") int teamId);
+   
 }
