f31aee8de35d4bc3a9edfd2959765338af628d39
ElakkiyaElamaran <153427532+ElakkiyaElamaran@users.noreply.github.com>
2025-09-11 18:12:58 +0530

    day 16

--- frontend/src/app/ipl/types/Team.ts
+++ frontend/src/app/ipl/types/Team.ts
@@ -1,48 +1,21 @@
-import { Team } from './Team';
 
-export class Match {
-    matchId: number;
-    // firstTeam: Team; // ManyToOne relationship with Team
-    // secondTeam: Team; // ManyToOne relationship with Team
-    firstTeamId:number;
-    secondTeamId:number;
-    matchDate: Date;
-    venue: string;
-    result: string;
-    status: string;
-    // winnerTeam: Team; // ManyToOne relationship with Team
-    winnerTeamId:number
 
-    constructor(
-        matchId: number,
-        // firstTeam: Team,
-        // secondTeam: Team,
-        firstTeamId:number,
-        secondTeamId:number,
-        matchDate: Date,
-        venue: string,
-        result: string,
-        status: string,
-        // winnerTeam: Team
-        winnerTeamId:number
-
-    ) {
-        this.matchId = matchId;
-        this.firstTeamId = firstTeamId;
-        this.secondTeamId = secondTeamId;
-        this.matchDate = matchDate;
-        this.venue = venue;
-        this.result = result;
-        this.status = status;
-        this.winnerTeamId = winnerTeamId;
-    }
-
-    displayInfo() {
-        console.log(`Match ID: ${this.matchId}`);
-        // console.log(`First Team: ${this.firstTeam.teamName}`);
-        // console.log(`Second Team: ${this.secondTeam.teamName}`);
-        // console.log(`Winner: ${this.winnerTeam.teamName}`);
-        console.log(`Match Date: ${this.matchDate}`)
-        console.log(`Venue: ${this.venue}`)
-    }
+export class Team {
+  teamId:number;
+  teamName:string;
+  location:string;
+  ownerName:string;
+  establishmentYear:number;
+  constructor(teamId:number,teamName:string,location:string,ownerName:string,establishmentYear:number){
+    this.teamId= teamId;
+    this.teamName =teamName;
+    this.location = location;
+    this.ownerName = ownerName;
+    this.establishmentYear = establishmentYear;
+  }
+  displayInfo():void{
+    console.log(`Team ID: ${this.teamId}`);
+    console.log(`Team Name: ${this.teamName}`);
+    console.log(`Location: ${this.location}`);
+  }
 }
